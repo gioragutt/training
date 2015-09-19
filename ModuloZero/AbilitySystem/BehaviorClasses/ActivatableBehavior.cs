b2f01985a4de75b45ec346273015f4ec60479c13
Giora Guttsait <giora111@gmail.com>
2015-09-19 23:24:57 +0300

    Fixed typo in ActivatableBehavior

--- ModuloZero/AbilitySystem/BehaviorClasses/ActivatableBehavior.cs
+++ ModuloZero/AbilitySystem/BehaviorClasses/ActivatableBehavior.cs
@@ -2,13 +2,13 @@
 {
     public abstract class ActivatableBehavior : IBehavior
     {
-        public abstract bool BehaviorImplentation(IUnit destinationPlayer);
+        public abstract bool BehaviorImplementation(IUnit destinationPlayer);
         public abstract bool CanApplyBehaviorTo(IUnit unit);
 
         public void ApplyBehavior(IUnit unit)
         {
             if (!CanApplyBehaviorTo(unit)) return;
-            BehaviorImplentation(unit);
+            BehaviorImplementation(unit);
         }
     }
 }
